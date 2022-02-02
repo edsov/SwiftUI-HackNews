751afafcf3991b3c767a1689067cc75186d7fe6b
Edgar Solis <edgarx75@gmail.com>
2022-02-02 14:29:58 -0600

    Initial Commit

---
+++ Hack News/Hack_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hack_NewsApp.swift
+//  Hack News
+//
+//  Created by Edgar Solis on 02/02/22.
+//
+
+import SwiftUI
+
+@main
+struct Hack_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
